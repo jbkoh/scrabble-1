f9ebd017f47982f942f3995bd35cbac4cbcee06e
Jason Koh <bk7749@gmail.com>
2018-08-13 14:59:14 -0700

    cleanup mongoengine schema

--- scrabble/data_model.py
+++ scrabble/data_model.py
@@ -3,21 +3,44 @@ from .common import *
 
 connect('plastering')
 
-
 class RawMetadata(Document):
     srcid = StringField(required=True)
     building = StringField(required=True)
-    metadata = DictField()
+    metadata = DictField() # This may contain any kind of metadata as a dictionary. There can be mutiple columns of the raw metadata.
+#    #example metadata
+#    {
+#        "VendorGivenName": "ZN",
+#        "BACnetDescription": "Zone"
+#    }
 
 class LabeledMetadata(Document):
     srcid = StringField(required=True)
     building = StringField(required=True)
     fullparsing = DictField()
-    tagsets = ListField(StringField())
-    point_tagset = StringField()
+#   #example full parsing
+#    {
+#        "VendorGivenName": [
+#            [ "Z", "B_zone"],
+#            [ "N", "I_zone"]
+#        ],
+#        "BACnetDescription": [
+#            [ "Z", "B_zone"],
+#            [ "o", "I_zone"],
+#            [ "n", "I_zone"],
+#            [ "e", "I_zone"]
+#        ]
+#    }
 
-column_names = ['VendorGivenName', 
-                 'BACnetName', 
+    tagsets = ListField(StringField())
+#   #example tagsets
+#    ["zone", ...]
+
+    point_tagset = StringField()
+#   #example point tagset
+#    "zone_temperature_sensor"
+
+column_names = ['VendorGivenName',
+                 'BACnetName',
                  'BACnetDescription']
 
 class ResultHistory(Document):
